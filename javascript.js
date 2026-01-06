450add61c6728f4f42e18a179296cf57215f590e
pizaje <199526115+pizaje@users.noreply.github.com>
2026-01-06 18:58:46 -0500

    add basic operation functions

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function mult(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(add(4, 5));
+console.log(sub(4, 5));
+console.log(mult(4, 5));
+console.log(divide(4, 5));
