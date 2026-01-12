cbd28b21ab7adbe896fed39a0ef5ddb49acb9b89
pizaje <199526115+pizaje@users.noreply.github.com>
2026-01-12 18:28:53 -0500

    add decimal support

--- javascript.js
+++ javascript.js
@@ -1,22 +1,26 @@
 let operator = "";
   let operatorinput = false;
 let hasOperated = false;
+let hasdecimal = false;
+  let completedecimal = true;
 let numbers = document.querySelectorAll("div.number");
 let signs = document.querySelectorAll("div.operator");
 let keys = document.querySelectorAll("div.button");
 let equal = document.querySelector("div#equal");
 let backspace = document.querySelector("div#delete");
 let ac = document.querySelector("div#ac");
+let period = document.querySelector("div#period");
 let input = document.querySelector("div#inputscreen");
 let result = document.querySelector("div#resultscreen");
 
 function equals () {
   let values = input.textContent.split(operator);
-  if ((values[0] != "" && operator === "") || (operator != "" && values[1] != "")) {
+  if (((values[0] != "" && operator === "") || (operator != "" && values[1] != "")) && completedecimal) {
     result.textContent = operate(parseFloat(values[0]), operator, parseFloat(values[1]));
     input.textContent = "";
     operator = "";
     hasOperated = false;
+    hasdecimal = false;
     operatorinput = false;
     equal.style.color = "#000";
   } else {
@@ -27,7 +31,8 @@ function equals () {
 for (button of signs) {
   let content = button;
   button.addEventListener("click", () => {
-    if (operatorinput && hasOperated === true) {
+  console.log(operator);
+    if (operatorinput && hasOperated === true && completedecimal) {
       equals();
       input.textContent = result.textContent;
     }
@@ -36,7 +41,7 @@ for (button of signs) {
     };
     operator = content.textContent;
     operatorless = input.textContent.slice(0, input.textContent.length - 1);
-    if (operatorinput && hasOperated === false) {
+    if ((operatorinput && hasOperated === false)) {
       input.textContent = operatorless;
     }
     operatorinput = true;
@@ -45,15 +50,29 @@ for (button of signs) {
 
 for (button of keys) {
   let content = button;
-  button.addEventListener("click", () => {
-    input.textContent += content.textContent;
-  });
+  if (button.classList.contains("number")) {
+    button.addEventListener("click", () => {
+      input.textContent += content.textContent;
+    });
+  }
+  if (button.classList.contains("operator")) {
+    button.addEventListener("click", () => {
+      if (completedecimal && operatorinput) {
+        input.textContent += content.textContent;
+        operatorinput = false;
+        hasdecimal = false;
+      }
+    });
+  }
 };
 
 for (button of numbers) {
   button.addEventListener("click", () => {
     if (operatorinput && !button.classList.contains("operator")) {
       hasOperated = true;
+    }
+    if (hasdecimal && button.id != "period") {
+      completedecimal = true;
     }
   })
 };
@@ -62,34 +81,46 @@ ac.addEventListener("click", () => {
   operator = "";
   operatorinput = false;
   hasOperated = false;
+  hasdecimal = false;
+  completedecimal = true;
   input.textContent = "";
   result.textContent = "0";
 });
 
 backspace.addEventListener("click", () => {
+  console.log(hasdecimal);
+  console.log(completedecimal);
   inputlen = input.textContent.length;
   back = input.textContent.slice(0, inputlen - 1);
+  perioddetect = input.textContent.slice(inputlen - 2, inputlen - 1);
+  console.log(perioddetect);
   deleted = input.textContent.slice(inputlen - 1, inputlen);
   if (deleted === "+" || deleted === "-" || deleted === "*" || deleted === "/" || deleted === "^") {
     operator = "";
     operatorinput = false;
     hasOperated = false;
   }
+  if (deleted === ".") {
+    hasdecimal = false;
+    completedecimal = true;
+  }
+  if (perioddetect === ".") {
+    completedecimal = false;
+  }
   input.textContent = back;
 })
-/*
-Pass a number, an operator, and a second number to a function
 
-- Wait for user to input a number into the calculator
-- If the user inputs a number, they may input an o9+perator
-  - If the user hasn't input a number, fill the input with the result's number (starts at 0)
-- If the user inputs an operator, set number1 = the number on the input and operator to the
-selected operator
-  - If another operator is selected again, replace the operator with the new one
-- After = is selected, set all that's to the right of the operator to hasOperated
-- Use the correct operation function on these values
-- Set all variables to 0, input screen to "", and result screen to 0 when AC is pressed
-*/
+period.addEventListener("click", () => {
+  let values = input.textContent.split(operator);
+  if (!hasdecimal) {
+    if ((values[0] === undefined && operator === "") || (operator != "" && values[1] === "")) {
+      input.textContent += 0;
+    }
+    input.textContent += ".";
+    hasdecimal = true;
+    completedecimal = false;
+  }
+});
 
 equal.addEventListener("click", equals); 
 
